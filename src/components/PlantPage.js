48cf0285c30c1b5bbcc6572eb559c5e54eb7e14a
Gil <gcastillohtx@gmail.com>
2022-02-09 08:38:53 -0600

    commit tree 25f81de59cb3bc64e71d45cd4405d7c40445868e

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -2,13 +2,43 @@ import React from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+import { useEffect, useState } from "react";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [filteredPlants, setFilteredPlants] = useState(plants);
+
+  function handleSearch(e) {
+    const filteredPlants = plants.filter((plant) => {
+      return plant.name.includes(e.target.value);
+    });
+
+    setFilteredPlants(filteredPlants);
+  }
+
+  function addNewPlant(newPlant){
+    const updatedPlants = [...plants, newPlant]
+    setPlants(updatedPlants)
+
+  }
+  useEffect(() => {
+    setFilteredPlants(plants)
+
+  }, [plants])
+  
+  
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((plantArray) => {
+        setPlants(plantArray)})
+  }, [])
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addNewPlant={addNewPlant}/>
+      <Search handleSearch={handleSearch} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
